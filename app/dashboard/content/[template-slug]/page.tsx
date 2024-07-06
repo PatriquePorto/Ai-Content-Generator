db11e485a571d4245d27be8ec6d69c905d0b545a
Patrique Porto <patrikrock@hotmail.com>
2024-07-06 08:55:45 -0300

    Layout update, add google gemini synch for search typed content

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 import React from 'react'
+import { useState } from 'react'
 import FormSection from '../_components/FormSection'
 import OutputSection from '../_components/OutputSection'
 import { TEMPLATE } from '../../_components/TemplateListSection'
 import Templates from '@/app/(data)/Templates'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { chatSession } from '@/utils/AiModal'
 
 interface PROPS {
 
@@ -16,22 +20,38 @@ interface PROPS {
 function CreateNewContent (props: PROPS)  {
 
     const selectedTemplate:TEMPLATE | undefined = Templates?.find((item) => item.slug == props.params['template-slug'])
+    const [loading, setLoading] = useState(false)
+    const [aiOutput, setAiOutput] = useState('')
+    const GenerateAIContent = async (formData:any) => {
+          setLoading(true)
+          const SelectedPrompt = selectedTemplate?.aiPrompt
 
-    const GenerateAIContent = (formData:any) => {
-          
+          const FinalAIPrompt=JSON.stringify(formData)+", "+SelectedPrompt
+
+          const result = await chatSession.sendMessage(FinalAIPrompt)
+
+          console.log(result.response.text())
+          setAiOutput(result?.response.text())
+          setLoading(false)
     }
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-5 p-5'>
-        {/* FormSection */}
-           <FormSection selectedTemplate={selectedTemplate}
-           userFormInput={(v:any)=> GenerateAIContent(v)}/>
+    <div className='p-10'>
+       <Link href={'/dashboard'}>
+          <Button>Back</Button>
+       </Link>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
+                {/* FormSection */}
+                <FormSection selectedTemplate={selectedTemplate}
+                userFormInput={(v:any)=> GenerateAIContent(v)}
+                loading={loading}/>
 
-        {/* OutputSection */}
-        <div className='col-span-2'>
-            <OutputSection/>
-        </div>
-           
+                {/* OutputSection */}
+                <div className='col-span-2'>
+                    <OutputSection aiOutput={aiOutput}/>
+                </div>
+                
+            </div>
     </div>
   )
 }
